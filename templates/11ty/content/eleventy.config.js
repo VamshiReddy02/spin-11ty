1cf5612de15c39265d77ce1e015a6bda7531b975
Vamshi Reddy <vamshiproject02@gmail.com>
2023-12-18 14:03:18 +0530

    added spin template for 11ty

--- templates/11ty/content/eleventy.config.js
+++ templates/11ty/content/eleventy.config.js
@@ -4,9 +4,7 @@ module.exports = function(eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy("src/assets");
     eleventyConfig.addPassthroughCopy("src/style.css");
-    eleventyConfig.addFilter("postDate", (dateObj) => {
-        return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
-    })
+  
 
     return{
         dir: {
